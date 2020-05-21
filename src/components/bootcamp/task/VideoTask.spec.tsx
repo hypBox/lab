2f75878728915fa6aa32937f0f15ecb6cc9e96f1
Amir Zad <am1hosseinzadeh@gmail.com>
2020-05-20 21:30:25 -0400

    add test

--- src/components/bootcamp/task/VideoTask.spec.tsx
+++ src/components/bootcamp/task/VideoTask.spec.tsx
@@ -1,22 +1,37 @@
 import React from "react"
 import renderer from "react-test-renderer"
-
 import VideoTask, { VideoTaskProps } from "./VideoTask"
+
+const completedHandler = jest.fn()
+
+jest.mock("react-player", () => {
+  const ReactPlayer = (props: any) => {
+    props.onProgress({ played: 100 })
+    return <div></div>
+  }
+  return ReactPlayer
+})
+
+const props: VideoTaskProps = {
+  onCompleted: completedHandler,
+  task: {
+    id: "t1",
+    title: "Bootcamp Overview",
+    type: "video",
+    url: "",
+    length: "1m 24s",
+    progress: 100,
+  },
+}
 
 describe("Video Task Component", () => {
   it("renders as expected", () => {
-    const props: VideoTaskProps = {
-      onCompleted: jest.fn(),
-      task: {
-        id: "t1",
-        title: "Bootcamp Overview",
-        type: "video",
-        url: "https://www.youtube.com/watch?v=gAprO6lGizM",
-        length: "1m 24s",
-        progress: 100,
-      },
-    }
     const root = renderer.create(<VideoTask {...props} />).toJSON()
     expect(root).toMatchSnapshot()
+  })
+
+  it("should call onCompleted callback when video completed", () => {
+    renderer.create(<VideoTask {...props} />)
+    expect(completedHandler).toBeCalled()
   })
 })
