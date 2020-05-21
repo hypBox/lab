cd05868a9b7135ee37364e95cd06e40e9f72490a
Amir Zad <am1hosseinzadeh@gmail.com>
2020-05-21 08:12:05 -0400

    add logo

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,11 @@ module.exports = {
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
+  moduleNameMapper: {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/__mocks__/fileMock.js",
+  },
   coverageThreshold: {
     global: {
       branches: 80,
