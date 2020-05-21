edf676fff809288ab2173bde0aaf2302ea48129d
Amir Zad <am1hosseinzadeh@gmail.com>
2020-05-21 19:41:35 -0400

    remove a mock file.

--- jest.config.js
+++ jest.config.js
@@ -11,8 +11,8 @@ module.exports = {
   },
   moduleNameMapper: {
     "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/fileMock.js",
-    "\\.(css|less)$": "<rootDir>/__mocks__/fileMock.js",
+      "<rootDir>/src/utility/assetsTransformer.js",
+    "\\.(css|less)$": "<rootDir>/src/utility/assetsTransformer.js",
   },
   coverageThreshold: {
     global: {
