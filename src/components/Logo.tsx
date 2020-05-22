74ac725ec3bfbc43033f86587fbf36e1c7503744
Amir Zad <am1hosseinzadeh@gmail.com>
2020-05-21 22:02:33 -0400

    fix prop issue and remove proptypes

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,23 +1,24 @@
 import React, { FunctionComponent } from "react"
 import { Avatar } from "./material/core"
-import logo from "../static/images/logo.png"
 import config from "../utility/config"
 
 export interface LogoProps {
   className?: string | undefined
 }
 
+const getLogoPath = () => `${process.env.PUBLIC_URL}/static/images/logo.png`
+
 const Logo: FunctionComponent<LogoProps> = (props) => {
   const {
     app: { slogan },
   } = config
+
   return (
     <Avatar
       alt={slogan}
       imgProps={{ title: slogan }}
       {...props}
-      src={logo}
-      sizes={logo}
+      src={getLogoPath()}
       variant="square"
     />
   )
