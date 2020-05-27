b2a5c14e272a5d9d220b79dbd0d585560efd9566
Amir Zad <am1hosseinzadeh@gmail.com>
2020-05-27 12:15:03 -0400

    add a test

--- src/components/bootcamp/task/Task.spec.tsx
+++ src/components/bootcamp/task/Task.spec.tsx
@@ -3,19 +3,30 @@ import renderer from "react-test-renderer"
 
 import Task, { TaskProps } from "./Task"
 
+const videoTask: TaskProps = {
+  task: {
+    id: "t1",
+    title: "Bootcamp Overview",
+    type: "video",
+    url: "https://www.youtube.com/watch?v=gAprO6lGizM",
+    length: "1m 24s",
+    progress: 100,
+  },
+}
+
+const renderTask = () => {
+  return renderer.create(<Task {...videoTask} />)
+}
+
 describe("Task Component", () => {
   it("renders as expected", () => {
-    const props: TaskProps = {
-      task: {
-        id: "t1",
-        title: "Bootcamp Overview",
-        type: "video",
-        url: "https://www.youtube.com/watch?v=gAprO6lGizM",
-        length: "1m 24s",
-        progress: 100,
-      },
-    }
-    const root = renderer.create(<Task {...props} />).toJSON()
-    expect(root).toMatchSnapshot()
+    const tree = renderTask().toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it("should return VideoTask component for task type video", () => {
+    const expectedTaskType = typeof (<Task {...videoTask} />)
+    const videoTaskComponentType = typeof renderTask()
+    expect(videoTaskComponentType).toBe(expectedTaskType)
   })
 })
